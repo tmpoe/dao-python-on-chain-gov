4d4e0a720ba0bc6b0c79e1cf89d5594cf8fb5b28
tmpITK <torok.mark.patrik@gmail.com>
2023-06-08 18:27:39 +0200

    Init brownie and add basis of gov contract

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# dao-python-on-chain-gov
+# Basic DAO in Python
+
+The basis of this repo is the 2021 Chainlink hackathon found here https://www.youtube.com/watch?v=rD8AxZ_wBA4
+
+We'll see where else I can go with this. Maybe will create a new repo after having enough fun with this and my own DAO from scratch.
---
+++ contracts/GovernanceToken.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+
+contract GovernanceToken is ERC20Votes {
+    uint256 public s_maxSupply = 1000000000000000000000000000; // 1 billion tokens
+    constructor()
+    ERC20("GovernanceToken", "GOV")
+    ERC20Permit("GovernanceToken")
+    {
+        _mint(msg.sender, s_maxSupply)
+    }
+}
