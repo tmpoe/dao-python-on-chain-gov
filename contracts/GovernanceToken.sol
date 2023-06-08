4f680ac223ae65973a14b8c810dcaae641167514
tmpITK <torok.mark.patrik@gmail.com>
2023-06-08 18:28:02 +0200

    FIx contract syntax

--- contracts/GovernanceToken.sol
+++ contracts/GovernanceToken.sol
@@ -6,10 +6,11 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 
 contract GovernanceToken is ERC20Votes {
     uint256 public s_maxSupply = 1000000000000000000000000000; // 1 billion tokens
+
     constructor()
-    ERC20("GovernanceToken", "GOV")
-    ERC20Permit("GovernanceToken")
+        ERC20("GovernanceToken", "GOV")
+        ERC20Permit("GovernanceToken")
     {
-        _mint(msg.sender, s_maxSupply)
+        _mint(msg.sender, s_maxSupply);
     }
 }
