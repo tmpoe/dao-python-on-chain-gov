f4b7497810493dad3785d01f2319df1d28703ce8
tmpITK <torok.mark.patrik@gmail.com>
2023-06-11 12:38:55 +0200

    Add queue and execute with test

--- scripts/governance.py
+++ scripts/governance.py
@@ -1,6 +1,9 @@
 from brownie import Contract, Box, MyGovernor
+from web3 import Web3
 
 from scripts.utils import get_account
+
+PROPOSAL_DESCRIPTION = "Change box value"
 
 
 def propose(box, new_value_to_store):
@@ -14,9 +17,48 @@ def propose(box, new_value_to_store):
         [box.address],
         [0],
         [encoded_box_store_func],
-        "Change box value",
+        PROPOSAL_DESCRIPTION,
         {"from": account},
     )
     proposal_tx.wait(1)
 
     return proposal_tx.events["ProposalCreated"]["proposalId"]
+
+
+def vote(proposal_id: int, vote: int):
+    # 0 = Against, 1 = For, 2 = Abstain
+    account = get_account()
+    tx = MyGovernor[-1].castVoteWithReason(
+        proposal_id, vote, "Want it changed", {"from": account}
+    )
+    tx.wait(1)
+
+
+def queue(store_value):
+    account = get_account()
+
+    tx = MyGovernor[-1].queue(
+        [Box[-1].address],
+        [0],
+        [get_encoded_function(store_value)],
+        Web3.keccak(text=PROPOSAL_DESCRIPTION).hex(),
+        {"from": account},
+    )
+    tx.wait(1)
+
+
+def get_encoded_function(store_value):
+    args = (store_value,)
+    return Contract.from_abi("Box", Box[-1], Box.abi).store.encode_input(*args)
+
+
+def execute(store_value):
+    account = get_account()
+    tx = MyGovernor[-1].execute(
+        [Box[-1].address],
+        [0],
+        [get_encoded_function(store_value)],
+        Web3.keccak(text=PROPOSAL_DESCRIPTION).hex(),
+        {"from": account},
+    )
+    tx.wait(1)
--- tests/test_governed_box.py
+++ tests/test_governed_box.py
@@ -1,8 +1,11 @@
+from time import sleep
 from brownie import Box, MyGovernor
 from scripts.config import DeployConfig
 
 from scripts.deploy import deploy_governed_box
-from scripts.governance import propose
+from scripts.governance import propose, queue, execute, vote
+
+TEST_BOX_VALUE = 42
 
 
 class TestDeployConfig(DeployConfig):
@@ -25,13 +28,13 @@ def test_governable_box_deploy():
 def test_governable_box_proposal_can_be_initiated():
     """
     GIVEN: A box
-    WHEN: Proposal is called on box to change its stored value to 42
+    WHEN: Proposal is called on box to change its stored value to TEST_BOX_VALUE
     THEN: The proposal is created and the stored value is not changed
     """
     deploy_governed_box(TestDeployConfig())
     box = Box[-1]
 
-    proposal_id = propose(box, 42)
+    proposal_id = propose(box, TEST_BOX_VALUE)
     assert proposal_id
     assert box.retrieve() == 0
     assert MyGovernor[-1].state(proposal_id) == 0  # pending
@@ -39,18 +42,24 @@ def test_governable_box_proposal_can_be_initiated():
     assert MyGovernor[-1].proposalSnapshot(proposal_id)
 
 
-def test_governable_box_voting(monkeypatch):
+def test_governable_box_proposal_executed(move_blocks):
     """
-    GIVEN: A box
-    WHEN: Proposal is called on box to change its stored value to 42
-    THEN: The proposal is created and the stored value is not changed
+    GIVEN: A succesful proposal
+    WHEN: Proposal is executed
+    THEN: The stored value is changed to TEST_BOX_VALUE
     """
-    deploy_governed_box(TestDeployConfig())
+    config = TestDeployConfig()
+    deploy_governed_box(config)
     box = Box[-1]
 
-    proposal_id = propose(box, 42)
-    assert proposal_id
-    assert box.retrieve() == 0
-    assert MyGovernor[-1].state(proposal_id) == 0  # pending
-    assert MyGovernor[-1].proposalDeadline(proposal_id)
-    assert MyGovernor[-1].proposalSnapshot(proposal_id)
+    proposal_id = propose(box, TEST_BOX_VALUE)
+    
+    move_blocks(2)
+    vote(proposal_id, 1)
+    move_blocks(config.VOTING_PERIOD)
+    
+    queue(TEST_BOX_VALUE)
+    sleep(config.MIN_DELAY)
+    execute(TEST_BOX_VALUE)
+    
+    assert box.retrieve() == TEST_BOX_VALUE
